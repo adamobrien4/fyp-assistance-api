0274a93faac563ad2043f340046c1e0f9f4c368b
Adam O'Brien <17226864@studentmail.ul.ie>
2021-03-02 21:04:29 +0000

    Heroku setup

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ db.once('open', () => {
 
 const app = require('./app')
 
-const port = process.version.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
